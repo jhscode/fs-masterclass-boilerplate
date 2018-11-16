75ec5f082fdc3363bec073ab74207bb441e54ca0
James Shin <jhscode@gmail.com>
2018-11-15 19:48:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  async componentDidMount () {
+    try {
+      const response = await axios.get('/healthcheck')
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+ 
   render() {
     return (
       <div className="App">
